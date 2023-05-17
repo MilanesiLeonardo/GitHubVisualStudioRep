bc50f004296716f1d031aaa3ab0271a5d6042dbc
MilanesiLeonardo <milanesi.leonardo.06@itisdalmine.edu.it>
2023-05-17 10:07:26 +0200

    Python Code Updated

--- main.py
+++ main.py
@@ -1,35 +1,187 @@
 import os
+from time import sleep
+
+def r1(rifiuto):
+    listaR1=[
+    "frigorifero",
+    "congelatore",
+    "condizionatore",
+    "deumidificatore",
+    "pompa di calore",
+    "radiatore a olio",
+    "asciugatrice con pompa di calore"]
+    if rifiuto in listaR1:
+        return True
+    return False
+
+
+def r2(rifiuto):
+    listaR2 = [
+    "lavatrice",
+    "lavastoviglie",
+    "apparecchio di cottura",
+    "stufa elettrica",
+    "piastra riscaldante"]
+    if rifiuto in listaR2:
+        return True
+    return False
+
+def r3(rifiuto):
+    listaR3 = [ 
+    "televisore",
+    "schermo",
+    "schermo lcd",
+    "monitor",
+    "laptop",
+    "notebook"
+    ]
+    if rifiuto in listaR3:
+        return True
+    return False
+
+def r4(rifiuto):
+    listaR4 = [
+    "mainframe",
+    "aspirapolvere",
+    "scopa meccanica",
+    "macchina per cucire",
+    "apparecchio di illuminazione",
+    "forno a microonde",
+    "ventilatore",
+    "ferro da stiro",
+    "tostapane",
+    "bollitore",
+    "sveglia",
+    "orologio",
+    "bilancia",
+    "calcolatrice",
+    "apparecchio radio",
+    "videocamera", 
+    "videoregistratore",
+    "giocattolo elettrico",
+    "giocattolo elettronico",
+    "rilevatore di fumo", 
+    "regolatore di calore", 
+    "termostato",
+    "apparecchio di cottura",
+    "stufa elettrica",
+    "piastra riscaldante",
+    "friggitrice", 
+    "frullatore", 
+    "macina caffè",
+    "asciugacapelli", 
+    "spazzolino da denti", 
+    "rasoio", 
+    "telefono",
+    "cellulare",
+    "navigatore satellitare",
+    "calcolatrice",
+    "router",
+    "pc",
+    "stampante",
+    "telefono"
+]
+    if rifiuto in listaR4:
+        return True
+    return False
+
+def r5(rifiuto):
+    listaR5 = [    
+    "tubi fluorescenti",
+    "lampade fluorescenti compatte",
+    "lampade fluorescenti",
+    "lampade a scarica ad alta densità",
+    "lampade a vapori di sodio ad alta pressione",
+    "lampade ad algoritmo metallico",
+    "lampade a vapori di sodio a bassa pressione",
+    "led"
+    ]
+    if rifiuto in listaR5:
+        return True
+    return False
 
 def gui(user):
     while True:
         os.system('cls')
         print(
 f"""
-+----------------------------------------+
++---------------------------------------------------------------+
+
             Progetto “RAEE”
 
     Benvenuto {user}, 
     Inserire la tipologia di
     apparecchio elettronico elettrico
-    da smaltire:
+    da smaltire (Deve essere singolare e non abbreviato es.Frigo):
+    
     """)
-        rifiuto = input()
+        rifiuto = input().lower()
         os.system('cls')
-        print(
-"""
-+----------------------------------------+            
+        if r1(rifiuto):
+            print(
+f"""
++---------------------------------------------------------------+
 
-    I RAEE sono suddivisi secondo il Dlgs n.49/2014 in 10 categorie:
+    {user}, il tuo rifiuto appartiene alla categoria R1 
 
-    R1) Grande bianco freddo -grandi elettrodomestici per la refrigerazione: frigoriferi, congelatori, condizionatori
-    R2) Grande bianco non freddo -grandi elettrodomestici come lavatrici, lavastoviglie.
-    R3) TV Monitor a tubo catodico
-    R4) Elettronica di consumo, Telecomunicazioni, Informatica, piccoli elettrodomestici, elettroutensili, 
-        giocattoli, apparecchi di illuminazione, dispositivi medici.
-    R5) Sorgenti luminose a scarica: lampade fluorescenti e sorgenti luminose compatte.
-            
-    Inserire la categoria più ideonea al rifiuto:
+    
 """)
-        categoria1 = input()
+            sleep(3)
+        elif r2(rifiuto):
+            print(
+f"""
++---------------------------------------------------------------+
+
+    {user}, il tuo rifiuto appartiene alla categoria R2 
+
+
+""")
+            sleep(3)
+        elif r3(rifiuto):
+            print(
+f"""
++---------------------------------------------------------------+
+
+    {user}, il tuo rifiuto appartiene alla categoria R3 
+
+
+""")
+            sleep(3)
+        elif r4(rifiuto):
+            print(
+f"""
++---------------------------------------------------------------+
+
+    {user}, il tuo rifiuto appartiene alla categoria R4
+
+
+""")
+            sleep(3)
+
+        elif r5(rifiuto):
+            print(
+f"""
++---------------------------------------------------------------+
+
+    {user}, il tuo rifiuto appartiene alla categoria R5
+
+
+""")
+            sleep(3)
+        else:
+            print(
+f"""
++---------------------------------------------------------------+
+
+    {user}, il tuo rifiuto non appartiene a nessuna categoria
+    controlla che l'inserimento sia corretto e rispetti
+    tutte le condizioni elencate
+
++---------------------------------------------------------------+
+""")
+            
+            sleep(6)
+        
+    
 user = input("Inserire un nome utente: ")
 gui(user)
